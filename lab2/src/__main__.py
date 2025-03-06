d0d6b0ba7bed9538dfccf1679aa4c6155786f834
fedorevgr <lpacwms3000@mail.ru>
2025-03-06 12:54:26 +0300

    newton works

--- lab2/src/__main__.py
+++ lab2/src/__main__.py
@@ -3,5 +3,10 @@ from .Multydimetia import MultidimensionalInterpolatorInator as MII
 from .Multydimetia import Method
 from pprint import pprint
 
+
+# todo tests
+
 pprint(ABOBA)
 pprint(MII.interp(ABOBA, [1.4, 1.4, 1.4], Method.Spline))
+pprint(MII.interp(ABOBA, [1.4, 1.4, 1.4], Method.Newton, 3))
+
